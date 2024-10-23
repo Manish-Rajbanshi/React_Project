7472a043d53ed6626eac199446bb8da2df7b7169
Manish_Rajbanshi <melishamelisha12@outlook.com>
2024-10-23 21:48:54 +0545

    vite updated

--- web-vite/src/App.jsx
+++ web-vite/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("blue")
 
   return (
     <>
+    <Navbar  title= "this is navbar"/>
+      {/* logo part */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,18 +19,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {/* logo part */}
+
+
+      <h1>New Vite WebSite</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setColor("red")}>
+        Click on here {color}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
